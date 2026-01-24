c449aa313803862f011f91b766aeda796bcbc16f
shawn <shawn.t.koshy@gmail.com>
2026-01-25 04:25:01 +0530

    added wrist or watever in uary leng

--- rover_stm_code/Core/Inc/uart.h
+++ rover_stm_code/Core/Inc/uart.h
@@ -18,7 +18,7 @@
 #define UART_RX_SIZE 2*NUM_MOTORS + NUM_SERVOS + WRIST_NUM_BYTES
 
 #define NUM_ACS 9 //4 in adc1 and 5 in adc2
-#define data_in_length (2*NUM_MOTORS) + NUM_SERVOS //input byte array
+#define data_in_length (2*NUM_MOTORS) + NUM_SERVOS + 1 //input byte array
 //OLD_FRAME
 //#define data_out_length 12*(NUM_QUAD + 1) + 2*NUM_ENCODERS + 2*NUM_ACS + 1
 
