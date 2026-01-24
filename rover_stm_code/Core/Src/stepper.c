0b0c5b5213568cdcd6adb9fa575f98ccec43de9f
Shridhar Patil <Shridhar.g.patil1@gmail.com>
2026-01-24 21:53:35 +0530

    changed stepper stuff

--- rover_stm_code/Core/Src/stepper.c
+++ rover_stm_code/Core/Src/stepper.c
@@ -24,26 +24,36 @@ void wrist_turn(uint8_t command){
 	switch(command){
 	case 0: //both forward
 		rotate = 1;
+		PCA9685_MOTOR_SetPWM(1, 0, 4095); //always on
+		PCA9685_MOTOR_SetPWM(2, 0, 4095);
 		HAL_GPIO_WritePin(DIR_STEP_1_GPIO_Port, DIR_STEP_1_Pin, 0);
 		HAL_GPIO_WritePin(DIR_STEP_2_GPIO_Port, DIR_STEP_2_Pin, 1);
 		break;
 	case 1: //both backwards
 		rotate = 1;
+		PCA9685_MOTOR_SetPWM(1, 0, 4095); //always on
+		PCA9685_MOTOR_SetPWM(2, 0, 4095);
 		HAL_GPIO_WritePin(DIR_STEP_1_GPIO_Port, DIR_STEP_1_Pin, 1);
 		HAL_GPIO_WritePin(DIR_STEP_2_GPIO_Port, DIR_STEP_2_Pin, 0);
 		break;
 	case 2: //turn
 		rotate = 1;
+		PCA9685_MOTOR_SetPWM(1, 0, 4095); //always on
+		PCA9685_MOTOR_SetPWM(2, 0, 4095);
 		HAL_GPIO_WritePin(DIR_STEP_1_GPIO_Port, DIR_STEP_1_Pin, 0);
 		HAL_GPIO_WritePin(DIR_STEP_2_GPIO_Port, DIR_STEP_2_Pin, 0);
 		break;
 	case 3: //turn
 		rotate = 1;
+		PCA9685_MOTOR_SetPWM(1, 0, 4095); //always on
+		PCA9685_MOTOR_SetPWM(2, 0, 4095);
 		HAL_GPIO_WritePin(DIR_STEP_1_GPIO_Port, DIR_STEP_1_Pin, 1);
 		HAL_GPIO_WritePin(DIR_STEP_2_GPIO_Port, DIR_STEP_2_Pin, 1);
 		break;
 	default: //do nothing
 		rotate = 0;
+		PCA9685_MOTOR_SetPWM(1, 4094, 4095); //always off
+		PCA9685_MOTOR_SetPWM(2, 4094, 4095);
 		break;
 	}
 }
@@ -55,16 +65,10 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 			step_pulse = !step_pulse;
 			HAL_GPIO_WritePin(PULSE1_GPIO_Port, PULSE1_Pin, step_pulse);
 			HAL_GPIO_WritePin(PULSE2_GPIO_Port, PULSE2_Pin, step_pulse);
-
-			PCA9685_MOTOR_SetPWM(13, 0, 4095); //always on
-			PCA9685_MOTOR_SetPWM(14, 0, 4095);
 		}
 		else{
 			HAL_GPIO_WritePin(PULSE1_GPIO_Port, PULSE1_Pin, 0);
 			HAL_GPIO_WritePin(PULSE2_GPIO_Port, PULSE2_Pin, 0);
-
-			PCA9685_MOTOR_SetPWM(13, 4094, 4095); //always off
-			PCA9685_MOTOR_SetPWM(14, 4094, 4095);
 		}
 	}
 
